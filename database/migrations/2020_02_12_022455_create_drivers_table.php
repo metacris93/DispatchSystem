bef18cf6146bea56c0e61b62c99745166ca4418d
Cristian Pisco <metacris93@gmail.com>
2020-02-12 00:43:58 -0500

    migrations and seeder added

---
+++ database/migrations/2020_02_12_022455_create_drivers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('lat', 11, 8);
+            $table->decimal('lng', 11, 8);
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
